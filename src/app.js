0e551a5ac65f4b3b87f04b586037d6a28449f10f
Oscar Lorite <olorite@uoc.edu>
2022-04-07 02:04:43 +0200

    Detall i revisió de responsive

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 console.log("Hello World");
 
+/** Gestió de l'acordió per ocultar dades */
+let desplegable = document.getElementsByClassName("desplegable");
+
+for(let i=0; i<desplegable.length;i++){
+    desplegable[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var desplegableText = this.nextElementSibling;
+        if (desplegableText.style.display === "block") {
+            desplegableText.style.display = "none";
+        } else {
+            desplegableText.style.display = "block";
+        }
+    });
+}
+
+
+
+
 /** Funcionalitat JS, llistem els personatges per categories 
  * a través d'una required i el mostrem editant el DOM de 
  * la pàgina.
  */
-const personatges = require('./resources/personatges-list.json');
-
+/*const personatges = require('./resources/personatges-list.json');
 for(let clau in personatges){
     //Personatges científics
     if(clau == 'cientifics'){
@@ -14,12 +31,15 @@ for(let clau in personatges){
             //Construim element contenidor del personatge
             let newElem = document.createElement("a");
             newElem.setAttribute("class","carrouselItem");
-            newElem.setAttribute("href",x.linkDetall);
+            newElem.setAttribute("href", x.linkPag);
             newElem.setAttribute("title","Fotografia de "+x.nom);
 
             //Construim element contenidor de la foto
             let newImg = document.createElement("img");
-            newImg.setAttribute("src",x.linkDetall);
+            //newImg.setAttribute("src", new URL(x.linkImg, import.meta.url));
+            
+            // on x.linkImg = "./resources/img/personatges/einstein.jpg"
+            //console.log(new URL("./resources/img/personatges/einstein.jpg", import.meta.url));
             newImg.setAttribute("alt","Imatge de "+x.nom);
 
             //Construim element contenidor de ltext
@@ -33,9 +53,9 @@ for(let clau in personatges){
 
             //Afegim nou element al contenidor flex 
             document.getElementById("cCientif").appendChild(newElem);
+
         })
     }
     
         
-};
-console.log(personatges);
+};*/
